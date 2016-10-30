a2f56e71c4e52797d3e02f6c02aa8ae06fd4d2ab
unknown <vimukthisaranga1@gmail.com>
2016-10-30 12:21:00 +0530

    menu added

--- AndroidApp/SCAT/app/src/main/java/com/example/pavsaranga/scat/Account.java
+++ AndroidApp/SCAT/app/src/main/java/com/example/pavsaranga/scat/Account.java
@@ -1,58 +1,74 @@
 package com.example.pavsaranga.scat;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ImageButton;
 
 /**
  * Created by P.A.V.Saranga on 29-Oct-16.
  */
-public class Account extends ListActivity {
+public class Account extends Activity {
 
-    String[] classes = {"View My Profile", "Check My Balance", "Share Credits", "view My Travel Info", "Block My Account", "Log Out"};
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        String item = classes[position];
-        try{
-            if(item.equals("View My Profile")){
-                Class loadClass = Class.forName("com.example.pavsaranga.scat.Map");
-                Intent loadIntent = new Intent(Account.this, loadClass);
-                startActivity(loadIntent);
-            } else if(item.equals("Check My Balance")) {
-                Class loadClass = Class.forName("com.example.pavsaranga.scat.Account");
-                Intent loadIntent = new Intent(Account.this, loadClass);
-                startActivity(loadIntent);
-            } else if(item.equals("Share Credits")) {
-                Class loadClass = Class.forName("com.example.pavsaranga.scat.Account");
-                Intent loadIntent = new Intent(Account.this, loadClass);
-                startActivity(loadIntent);
-            } else if(item.equals("view My Travel Info")) {
-                Class loadClass = Class.forName("com.example.pavsaranga.scat.Account");
-                Intent loadIntent = new Intent(Account.this, loadClass);
-                startActivity(loadIntent);
-            } else if(item.equals("Block My Account")) {
-                Class loadClass = Class.forName("com.example.pavsaranga.scat.Account");
-                Intent loadIntent = new Intent(Account.this, loadClass);
-                startActivity(loadIntent);
-            } else if(item.equals("Log Out")) {
-                Class loadClass = Class.forName("com.example.pavsaranga.scat.Account");
-                Intent loadIntent = new Intent(Account.this, loadClass);
-                startActivity(loadIntent);
-            }
-        } catch(ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+    private ImageButton btn1, btn2, btn3, btn4, btn5, btn6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(Account.this, android.R.layout.simple_list_item_1, classes));
+        setContentView(R.layout.account);
+        setUp();
+    }
+
+    protected void setUp(){
+        btn1 = (ImageButton) findViewById(R.id.button01);
+        btn2 = (ImageButton) findViewById(R.id.button02);
+        btn3 = (ImageButton) findViewById(R.id.button03);
+        btn4 = (ImageButton) findViewById(R.id.button04);
+        btn5 = (ImageButton) findViewById(R.id.button05);
+        btn6 = (ImageButton) findViewById(R.id.button06);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i1 = new Intent("com.example.pavsaranga.scat.PROFILE");
+                startActivity(i1);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i2 = new Intent("com.example.pavsaranga.scat.BALANCE");
+                startActivity(i2);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i3 = new Intent("com.example.pavsaranga.scat.TRAVEL");
+                startActivity(i3);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i4 = new Intent("com.example.pavsaranga.scat.TRANSFER");
+                startActivity(i4);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i5 = new Intent("com.example.pavsaranga.scat.DISABLE");
+                startActivity(i5);
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i6 = new Intent("com.example.pavsaranga.scat.LOGOUT");
+                startActivity(i6);
+            }
+        });
     }
 }
 
---
+++ AndroidApp/SCAT/app/src/main/java/com/example/pavsaranga/scat/Balance.java
@@ -0,0 +1,16 @@
+package com.example.pavsaranga.scat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by P.A.V.Saranga on 30-Oct-16.
+ */
+public class Balance extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.balance);
+    }
+}
